72d4bd5d521ef1c7666f3c516f8f0b12d27144fc
Ricardo Oliveira <ricvenicius@gmail.com>
2024-11-29 18:56:25 -0300

    Fix: auto restart server with run.py

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
+import os
+
 from app.main import app
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    os.environ["FLASK_APP"] = "run.py"
+    os.environ["FLASK_ENV"] = "development"
     app.run(host="0.0.0.0", port=5000, debug=True)
