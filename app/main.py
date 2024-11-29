065c0a8c6021a660e364d06471d2e18feea8349e
Ricardo Oliveira <ricvenicius@gmail.com>
2024-11-29 18:55:11 -0300

    Fix: mongo conection

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import mongoengine as db
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
 db.connect(
-    host="mongodb://admin:admin-password@mongodb:27017/user_db"
+    host="mongodb://admin:admin-password@mongodb:27017/userdb?authSource=admin"
 )
 
 
@@ -17,10 +17,21 @@ class User(db.Document):
     last_name = db.StringField(required=True)
     birth_date = db.DateTimeField(required=True)
 
+    def __repr__(self):
+        return {
+            "cpf": self.cpf,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "birth_date": self.birth_date,
+        }
+
 
 class HelloWorld(Resource):
     def get(self):
-        return {"Hello": "World"}
+        users = [user.to_mongo().to_dict()
+                 for user in User.objects()]  # type: ignore
+        return jsonify(users)
 
 
 api.add_resource(HelloWorld, "/")
