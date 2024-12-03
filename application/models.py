fc15f71289edabe0037e9d90be0a58836207c6b8
Ricardo Oliveira <ricvenicius@gmail.com>
2024-12-03 09:35:55 -0300

    feat: add object and query methods to User class

--- application/models.py
+++ application/models.py
@@ -3,11 +3,30 @@ from flask_restful import reqparse
 
 
 class User(db.Document):
-    cpf = db.StringField(required=True)
+    cpf = db.StringField(required=True, unique=True)
     email = db.EmailField(required=True)
     first_name = db.StringField(required=True)
     last_name = db.StringField(required=True)
     birth_date = db.DateTimeField(required=True)
+
+    def repr(self):
+        return {
+            "cpf": self.cpf,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "birth_date": self.birth_date
+        }
+
+    @classmethod
+    def find_by_cpf(cls, cpf):
+        user = cls.objects(cpf=cpf)[0]
+        return user.repr()
+
+    @classmethod
+    def all_users(cls):
+        users = [user.repr() for user in cls.objects()]
+        return users
 
 
 parser = reqparse.RequestParser()
